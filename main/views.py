eae11d9ac1ce8b3e42c95fcef8376969ee77e546
Akzyinat <vip.akzyinat@gmail.com>
2021-01-29 21:51:37 +0600

    added second test page and url

--- main/views.py
+++ main/views.py
@@ -5,5 +5,8 @@ def homepage(request):
     return HttpResponse("Hello world!")
 
 
-def test (request):
+def test(request):
     return render(request, "test.html")
+
+def second(request):
+    return HttpResponse("test 2 page")
